ffc5d110025fe6318c753e9d98cb9edcd9325ea6
JeanineMac4 <fanlingjie@jeanine.cn>
2015-12-08 16:20:47 +0800

    rm stuff

--- target/cljsbuild/public/js/out/luminus_demo/todo.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {:static-fns true, :optimize-constants true}
-goog.provide('luminus_demo.todo');
-goog.require('cljs.core');
-goog.require('reagent.core');
-luminus_demo.todo.todo_page = (function luminus_demo$todo$todo_page(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$container,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$jumbotron,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$h1,"Big Data"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$p,"Collectiong more info"], null)], null)], null);
-});
