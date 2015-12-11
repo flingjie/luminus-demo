2c36be44b27f12fd72d07c97dac084ec647696ad
amazon <fanglingjie.cn@gmail.com>
2015-12-11 15:46:07 +0000

    update project

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.webjars/bootstrap "3.3.5"]
                  [org.webjars/jquery "2.1.4"]
-                 [org.webjars/bootstrap-autohidingnavbar "1.0.4"]
+                 [org.webjars.bower/bootstrap-autohidingnavbar "1.0.4"]
                  [com.fzakaria/slf4j-timbre "0.2.1"]
                  [org.clojure/clojurescript "1.7.170" :scope "provided"]
                  [reagent "0.5.1"]
