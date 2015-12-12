1fb9916c668f552061569ca6e09b91a78462224d
JeanineMac4 <fanlingjie@jeanine.cn>
2015-12-12 15:46:11 +0800

    add cljhttp

--- project.clj
+++ project.clj
@@ -31,6 +31,7 @@
                  [reagent-utils "0.1.5"]
                  [secretary "1.2.3"]
                  [org.clojure/core.async "0.2.371"]
+                 [clj-http "2.0.0"]
                  [cljs-ajax "0.5.1"]
                  [org.immutant/web "2.1.0"]
                  [prismatic/dommy "1.1.0"]]
