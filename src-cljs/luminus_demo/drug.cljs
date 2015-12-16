6aca3a8725f41698d6a091d5a8deb4e192b2aa68
JeanineMac4 <fanlingjie@jeanine.cn>
2015-12-16 19:38:55 +0800

    add loading

--- src-cljs/luminus_demo/drug.cljs
+++ src-cljs/luminus_demo/drug.cljs
@@ -1,21 +1,22 @@
 (ns luminus-demo.drug
   (:require [reagent.core :as r :refer [atom]]
             [ajax.core :refer [GET POST]]
-            [dommy.core :refer-macros [sel1]]
+            [dommy.core :as dommy :refer-macros [sel1]]
             [secretary.core :as secretary :include-macros true]
             [reagent.session :as session]
             ))
 
 (defn drug-info [name img_url detail_url]
   [:div
-   [:a {:href detail_url :target "_blank"} name]
-   [:a {:href detail_url :target "_blank"}
-    [:img {:src img_url}]]])
+   [:a {:href detail_url} name]
+   [:a {:href detail_url}
+    [:img.drug-img {:src img_url}]]])
 
 (defn drug-list [drugs]
-  [:div
+  [:div.col-lg-2
    (for [drug drugs]
-     [drug-info (.-name drug) (.-img drug) " "])])
+     ^{:key (.-id drug)}
+     [drug-info (.-name drug) (.-img drug) (.-url drug)])])
 
 (defn drug-debug [drugs]
   (.log js/console "drugs")
@@ -53,31 +54,48 @@
 
 ;; -------------------------
 ;; Handlers
+(defn reset-btn []
+  (dommy/remove-class! (sel1 :#loading-div) :loading)
+  (->
+    (sel1 :#search-btn)
+    (dommy/remove-attr! :disabled)))
+
+(defn show-loading []
+  (dommy/add-class! (sel1 :#loading-div) :loading)
+  (->
+   (sel1 :#search-btn)
+   (dommy/set-attr! :disabled)))
 
 (defn handler [response]
   (let [items (.-tngou (clj->js response))]
     (r/render [#'drug-list items] (sel1 :#result))
+    (reset-btn)
     ))
 
 (defn error-handler [{:keys [status status-text]}]
-  (.log js/console (str "error: " status " " status-text)))
+  (.log js/console (str "error: " status " " status-text))
+  (reset-btn))
 
 ;; -------------------------
 
-
 (defn query []
   (let [csrf (.-value (sel1 :#token))
-        keyword (.-value (sel1 :#keyword))]
+        search (.-value (sel1 :#keyword))]
     (POST "/"
           {:headers {:x-CSRF-Token csrf}
-           :params {:keyword keyword}
+           :params {:keyword search}
            :response-format :json
            :handler handler
            :error-handler error-handler})
-     ))
+    (show-loading)
+    )
+  )
 
 (defn query-div []
   (fn []
     [:div.text-center
-     [:input {:type "text" :id "keyword"}]
-     [:button.btn-primary {:on-click query} "药物查询" ]]))
+     [:div.div-beside
+      [:input {:type "text" :id "keyword"}]
+     [:button#search-btn.btn-primary.btn-search {:on-click query} "药物查询"]]
+     [:div#loading-div.div-beside]
+     ]))
